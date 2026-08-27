9786d7817f9c10700a3dd2e75724d716a6170904
Jakob Willforss <jakob.willforss@hotmail.com>
2026-08-27 20:06:06 +0200

    Initial hello world electron setup

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 900,
+    height: 620,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true,
+    },
+  });
+
+  window.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
---
+++ preload.js
@@ -0,0 +1,2 @@
+// Keep privileged Electron/Node APIs out of the renderer by default.
+// Add a deliberately small API here when the UI needs native capabilities.
